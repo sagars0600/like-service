1ddcc312716f2bcbffbf868c598c949ab103145a
senil <senila@maveric-systems.com>
2022-03-22 15:20:33 +0530

    getByDetailLikeId

--- src/main/java/com/likeservice/likeservice/controller/LikeController.java
+++ src/main/java/com/likeservice/likeservice/controller/LikeController.java
@@ -1,7 +1,12 @@
 package com.likeservice.likeservice.controller;
 
+import com.likeservice.likeservice.model.Like;
 import com.likeservice.likeservice.service.LikeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +14,9 @@ public class LikeController {
 
     @Autowired
     private LikeService likeService;
+
+    @GetMapping("/postsOrComments/{postOrCommentId}/likes/{likeId}")
+    public ResponseEntity<Like> likeDetailsOnID(@PathVariable("likeId") String likeId, @PathVariable("postOrCommentId") String postOrCommentId){
+        return new ResponseEntity<>(likeService.likeDetailsOnID(likeId), HttpStatus.ACCEPTED);
+    }
 }
