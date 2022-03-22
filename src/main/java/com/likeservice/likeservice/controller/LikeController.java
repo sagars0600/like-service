851440ee2609567190e9f4d0d7c60c34bf6b1397
senil <senila@maveric-systems.com>
2022-03-22 14:52:48 +0530

    add like

--- src/main/java/com/likeservice/likeservice/controller/LikeController.java
+++ src/main/java/com/likeservice/likeservice/controller/LikeController.java
@@ -1,12 +1,24 @@
 package com.likeservice.likeservice.controller;
 
+import com.likeservice.likeservice.model.Like;
 import com.likeservice.likeservice.service.LikeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class LikeController {
 
     @Autowired
     private LikeService likeService;
+    @PostMapping("/postsOrComments/{postOrCommentId}/likes")
+    public ResponseEntity<Like> createLike(@PathVariable("postOrCommentId") String postOrCommentId, @RequestBody @Valid Like like){
+        return new ResponseEntity<>(likeService.createLike(like,postOrCommentId), HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/com/likeservice/likeservice/service/LikeService.java
+++ src/main/java/com/likeservice/likeservice/service/LikeService.java
@@ -1,13 +1,22 @@
 package com.likeservice.likeservice.service;
 
+import com.likeservice.likeservice.model.Like;
 import com.likeservice.likeservice.repo.LikeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class LikeService {
 
     @Autowired
     private LikeRepo likeRepo;
+    public Like createLike(Like like, String postOrCommentId){
+        like.setPostOrCommentId(postOrCommentId);
+        like.setCreatedAt(LocalDateTime.now());
+        return this.likeRepo.save(like);
+
+    }
 
 }
